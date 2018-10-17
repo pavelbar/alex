dda85f2c6a18b33fa11d5c4e58f580b0faf11f4f
Baranov Pavel <pavelbaranow@gmail.com>
2018-10-17 15:46:49 +0300

    Create 7.c

---
+++ 7.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <windows.h>
+
+/*
+ * Известны два расстояниея. Одно в КМ другое в футах. (1км = 0.45 фута). Какое расстояние меньше?
+ */
+
+int correctly(char nameVariable) {
+    system("cls");
+    printf("MODE: Input INT Variable %c\n\n%c = ", nameVariable, nameVariable);
+    int flag = 0;
+    int answer = 0;
+
+    while (1) {
+        flag = scanf("%d", &answer);
+        fflush(stdin);
+        if ((flag == 1) && (answer > 0)) {
+            printf("Variable %c correctly! \n\n", nameVariable);
+            break;
+
+        } else {
+            printf("Variable %c not correctly! Try again. \n%c = ", nameVariable, nameVariable);
+        }
+    }
+    printf("Press ENTER to normal mode");
+    getchar();
+    system("cls");
+    return answer;
+}
+
+
+void compareDistances(int km, int foot) {       //Task7
+    double distanceTwoKm = foot * 0.45;
+
+    printf("\n distance 2 (KM) = %f\n", distanceTwoKm);
+
+    if (km < distanceTwoKm) {
+        puts(" distance 1 < distance 2");
+    } else {
+        puts(" distance 1 >= distance 2");
+    }
+}
+
+
+int main() {
+    while (1) {
+        double distanceOneKm = 0;
+        printf("Enter distance 1 (Km) (int>=0)\n\n");
+        printf("Press ENTER to input mode");
+        getchar();
+        distanceOneKm = correctly('A');
+
+        double distanceTwoFoot = 0;
+        printf("Enter distance 2 (Foot) (int>=0)\n\n");
+        printf("Press ENTER to input mode");
+        getchar();
+        distanceTwoFoot = correctly('B');
+
+        system("cls");
+        printf("distance 1 (KM) = %f\ndistance 2 (Foot) = %f\n", distanceOneKm, distanceTwoFoot);
+
+        compareDistances(distanceOneKm, distanceTwoFoot);
+
+        printf("\nJob done. Press ENTER");
+        getchar();
+        system("cls");
+    }
+    return 0;
+}
