7edc3d71c008f636a95d30395728d2df31e4c7db
Baranov Pavel <pavelbaranow@gmail.com>
2018-10-17 17:34:00 +0300

    Create 16.c

---
+++ 16.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <windows.h>
+
+/*
+ * Дано трехзначное число. Выяснить, является ли оно палиндромом («перевертышем»),
+ * то есть числом, десятичная запись которого читается одинаково слева направо и справа налево
+ */
+
+int getLengthNumber(int numb) {
+    int count = 0;
+    while (numb) {
+        numb /= 10;
+        count++;
+    }
+    return count;
+}
+
+
+int correctly(char nameVariable) {
+    system("cls");
+    printf("MODE: Input INT Variable %c\n\n%c = ", nameVariable, nameVariable);
+    int flag = 0;
+    int answer = 0;
+
+    while (1) {
+        flag = scanf("%d", &answer);
+        fflush(stdin);
+        if (flag == 1) {
+            if (getLengthNumber(answer) == 3) {
+                printf("Variable %c correctly! \n\n", nameVariable);
+                break;
+            } else {
+                printf("Variable %c not correctly! Try again. \n%c = ", nameVariable, nameVariable);
+            }
+
+        } else {
+            printf("Variable %c not correctly! Try again. \n%c = ", nameVariable, nameVariable);
+        }
+    }
+    printf("Press ENTER to normal mode");
+    getchar();
+    system("cls");
+    return answer;
+}
+
+
+void isPalindrome(int num) { //Task 16
+    int rightDigit = abs(num) % 10;
+    int leftDigit = abs(num) / 100;
+
+
+    printf(" Left Digit =%d\n", leftDigit);
+    printf(" Right Digit =%d\n", rightDigit);
+
+
+    if (rightDigit == leftDigit) {
+        puts(" All right. Digit is palindrome");
+    } else {
+        puts(" Error. Digit is NOT palindrome");
+    }
+}
+
+
+int main() {
+    while (1) {
+        int num = 0;
+        printf("Enter digit (int, length = 2)\n\n");
+        printf("Press ENTER to input mode");
+        getchar();
+        num = correctly('A');
+
+        system("cls");
+        printf("digit = %d\n", num);
+
+        isPalindrome(num);
+
+        printf("\nJob done. Press ENTER");
+        getchar();
+        system("cls");
+    }
+    return 0;
+}
