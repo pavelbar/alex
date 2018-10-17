7051fa5123d84f0e7feea62637e962d97e9283e5
Baranov Pavel <pavelbaranow@gmail.com>
2018-10-17 16:44:58 +0300

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <windows.h>
+
+/*
+ * Дано двухзначное число. Определить какая из его цифр больше 1 или 2
+ */
+
+int getLengthNumber(int numb) {
+    int count = 0;
+    while (numb) {
+        numb /= 10;
+        count++;
+    }
+    return count;
+}
+
+
+int correctly(char nameVariable) {
+    system("cls");
+    printf("MODE: Input INT Variable %c\n\n%c = ", nameVariable, nameVariable);
+    int flag = 0;
+    int answer = 0;
+
+    while (1) {
+        flag = scanf("%d", &answer);
+        fflush(stdin);
+        if (flag == 1) {
+            if (getLengthNumber(answer) == 2) {
+                printf("Variable %c correctly! \n\n", nameVariable);
+                break;
+            } else {
+                printf("Variable %c not correctly! Try again. \n%c = ", nameVariable, nameVariable);
+            }
+
+        } else {
+            printf("Variable %c not correctly! Try again. \n%c = ", nameVariable, nameVariable);
+        }
+    }
+    printf("Press ENTER to normal mode");
+    getchar();
+    system("cls");
+    return answer;
+}
+
+
+void compareFirstAndLastDigit(int num) { //Task 11
+    int rightDigit = abs(num) % 10;
+    int leftDigit = abs(num) / 10;
+
+    printf(" Left Digit =%d\n", leftDigit);
+    printf(" Right Digit =%d\n", rightDigit);
+
+    if (leftDigit < rightDigit) {
+        puts(" Left Digit < Right Digit");
+    } else {
+        puts(" Left Digit >= Right Digit");
+    }
+}
+
+
+int main() {
+    while (1) {
+        int num = 0;
+        printf("Enter digit (int, length = 2)\n\n");
+        printf("Press ENTER to input mode");
+        getchar();
+        num = correctly('A');
+
+        system("cls");
+        printf("digit = %d\n", num);
+
+        compareFirstAndLastDigit(num);
+
+        printf("\nJob done. Press ENTER");
+        getchar();
+        system("cls");
+    }
+    return 0;
+}
