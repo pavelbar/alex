62c267449517bfc78a247fed5d2771e9e41d098a
Baranov Pavel <pavelbaranow@gmail.com>
2018-10-19 19:05:26 +0300

    Update 13.c

--- 13.c
+++ 13.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <windows.h>
+#include <math.h>
 
 /*
  * Дано двузначное число. Определить, равен ли квадрат этого числа учетверенной сумме кубов его цифр.
@@ -48,13 +49,7 @@ int getLastDigit(int number) {
 
 
 int getFirstDigit(int number) {
-    number = abs(number);
-    int first = 0;
-    while (number > 0) {
-        first = number % 10;
-        number = number / 10;
-    }
-    return first;
+    return number / pow(10, getLengthNumber(number) - 1);
 }
 
 void solution(int num) { //Task 13
