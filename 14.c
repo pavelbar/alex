477822678161d93ed3ab5fea215f5ce6e78b4cfb
Baranov Pavel <pavelbaranow@gmail.com>
2018-10-19 19:05:02 +0300

    Update 14.c

--- 14.c
+++ 14.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <windows.h>
+#include <math.h>
 
 /*
  * Дано двузначное число. Определить, является ли сумма его цифр двузначным числом
@@ -48,13 +49,7 @@ int getLastDigit(int number) {
 
 
 int getFirstDigit(int number) {
-    number = abs(number);
-    int first = 0;
-    while (number > 0) {
-        first = number % 10;
-        number = number / 10;
-    }
-    return first;
+    return number / pow(10, getLengthNumber(number) - 1);
 }
 
 
