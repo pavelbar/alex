5b07694684489e540dbd86b27d6512686c558b94
Baranov Pavel <pavelbaranow@gmail.com>
2018-10-19 19:32:14 +0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -5,8 +5,16 @@
  * Является ли число А делителем Б
  */
 
-int correctly(char nameVariable) {
+void myCls() {
     system("cls");
+    puts("************************************************************\n"
+         "*Nizhniy Novgorod Technical University*\n*Study work number 2. Task number 1."
+         "*\n *Performed student 18 - IVT - 2. Kornilov Alexey "
+         "*\n************************************************************\n");
+}
+
+int correctly(char nameVariable) {
+    myCls();
     printf("MODE: Input INT Variable %c\n\n%c = ", nameVariable, nameVariable);
     int flag = 0;
     int answer = 0;
@@ -27,7 +35,6 @@ int correctly(char nameVariable) {
     }
     printf("Press ENTER to normal mode");
     getchar();
-    system("cls");
     return answer;
 }
 
@@ -41,26 +48,27 @@ void isDivider(int A, int B) {       //Task 2
 
 int main() {
     while (1) {
+        myCls();
         int B = 0;
         printf("Enter dividend B (int)\n\n");//делимое
         printf("Press ENTER to input mode");
         getchar();
         B = correctly('B');
 
+        myCls();
         int A = 0;
         printf("Enter divider A (int)\n\n");//делитель
         printf("Press ENTER to input mode");
         getchar();
         A = correctly('A');
 
-        system("cls");
+        myCls();
         printf("A = %d\nB = %d \n B / A", A, B);
 
         isDivider(A, B);
 
         printf("\nJob done. Press ENTER");
         getchar();
-        system("cls");
     }
     return 0;
 }
