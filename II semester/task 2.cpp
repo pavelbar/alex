95dbd0fcd53bf10073dea2c571a83c5b0adb7b43
Baranov Pavel <pavelbaranow@gmail.com>
2019-03-17 15:46:29 +0300

    Update task 2.cpp

--- II semester/task 2.cpp
+++ II semester/task 2.cpp
@@ -5,8 +5,22 @@
 #include <vector> 
 #include <string>
 
-
 using namespace std;
+
+struct TicketOffice
+{
+	string ticketNumber;//номер кассы
+	string fullNameCashier;//ФИО кассира
+	string amountSoldTickets;//Кол-во проанных билетов
+	string totalRevenue;//Суммарная выручка
+	string salesDate;//Дата продажи
+
+	const string nameTicketNumber = "Ticket number: ";
+	const string nameFullNameCashier = "Full name cashier: ";
+	const string nameAmountSoldTickets = "Amount sold tickets: ";
+	const string nameTotalRevenue = "Total revenue: ";
+	const string nameSalesDate = "Sales date: ";
+};
 
 void getHelp() {
 	cout << endl << "========================HELP=======================";//Правила использования, цель назначения иинформацию о разработчике
@@ -43,10 +57,10 @@ void parserCliArguments(int argc, char** argv, string &fileName, int &N, string 
 							int countDelim = count(subject.begin(), subject.end(), delim);
 							if (countDelim == 1) {
 								fileName = subject.substr(1 + subject.find(delim));
-								string tmpN = subject.substr(0, subject.find(delim) );
+								string tmpN = subject.substr(0, subject.find(delim));
 								N = atoi(tmpN.c_str());
-								if(vAllCliArguments[i] == "-c"){
-								mode = "create";
+								if (vAllCliArguments[i] == "-c") {
+									mode = "create";
 								}
 								else {
 									mode = "read";
@@ -89,40 +103,75 @@ void parserCliArguments(int argc, char** argv, string &fileName, int &N, string 
 	}
 }
 
-void createMode(string fileName) {
-	cout << endl << "=C=R=E=A=T=E= =M=O=D=E" << endl;
+vector<TicketOffice> inputMode(string fileName, int N) {
+	vector<TicketOffice> vMyTicketOffice;
+	cout << endl << endl << "-> =I=N=P=U=T= =M=O=D=E";
+	cout << endl << "ENTER FROM KEYBOARD:" << endl;
+		for (int i = 0; i < N; i++) {
+			TicketOffice elt;
 
-	std::ofstream myStream;
-	myStream.open(fileName);
-	if (myStream.is_open()) {
-		cout << endl << "ENTER FROM KEYBOARD:" << endl;
-		while (1) {
-			char symbol = getchar();            // Get one character from the input
-			if (symbol == EOF) {
-				break;
-			}  // Exit the loop if we receive EOF ("end of file")
-			myStream << symbol;
+			cout << endl << "Serial number " << i + 1 << "(of " <<N <<")" ;
+
+			cout << endl  << elt.nameTicketNumber;
+			cin >> elt.ticketNumber;
+
+			cout << elt.nameFullNameCashier;
+			cin >> elt.fullNameCashier;
+
+			cout  << elt.nameAmountSoldTickets;
+			cin >> elt.amountSoldTickets;
+
+			cout  << elt.nameTotalRevenue;
+			cin >> elt.totalRevenue;
+
+			cout  << elt.nameSalesDate;
+			cin >> elt.salesDate;
+
+			vMyTicketOffice.push_back(elt);
 		}
-	}
-	myStream.close();
+		return vMyTicketOffice;
 }
 
-void readMode(string fileName) {
+void printMode(vector<TicketOffice> vMyTicketOffice) {
+	cout << endl << "-> =P=R=I=N=T= =M=O=D=E" ;
+
+	for (int i = 0; i < vMyTicketOffice.size(); i++) {
+		cout << endl << endl << "Serial number " << i + 1 << "(of " << vMyTicketOffice.size() << ")";
+
+		cout << endl << vMyTicketOffice[i].nameTicketNumber;
+		cout << vMyTicketOffice[i].ticketNumber;
+
+		cout << endl << vMyTicketOffice[i].nameFullNameCashier;
+		cout << vMyTicketOffice[i].fullNameCashier;
+
+		cout << endl << vMyTicketOffice[i].nameAmountSoldTickets;
+		cout << vMyTicketOffice[i].amountSoldTickets;
+
+		cout << endl << vMyTicketOffice[i].nameTotalRevenue;
+		cout << vMyTicketOffice[i].totalRevenue;
+
+		cout << endl << vMyTicketOffice[i].nameSalesDate;
+		cout << vMyTicketOffice[i].salesDate;
+
+	}
+}
+
+void readMode(string fileName, vector<TicketOffice> &vMyTicketOffice, int N) {
 	cout << endl << "=R=E=A=D= =M=O=D=E" << endl;
 
-	std::ofstream myStream;
-	myStream.open(fileName);
-	if (myStream.is_open()) {
-		cout << endl << "ENTER FROM KEYBOARD:" << endl;
-		while (1) {
-			char symbol = getchar();            // Get one character from the input
-			if (symbol == EOF) {
-				break;
-			}  // Exit the loop if we receive EOF ("end of file")
-			myStream << symbol;
-		}
-	}
-	myStream.close();
+	//std::ofstream myStream;
+	//myStream.open(fileName);
+	//if (myStream.is_open()) {
+	//	cout << endl << "ENTER FROM KEYBOARD:" << endl;
+	//	while (1) {
+	//		char symbol = getchar();            // Get one character from the input
+	//		if (symbol == EOF) {
+	//			break;
+	//		}  // Exit the loop if we receive EOF ("end of file")
+	//		myStream << symbol;
+	//	}
+	//}
+	//myStream.close();
 }
 
 int main(int argc, char** argv) {
@@ -134,11 +183,15 @@ int main(int argc, char** argv) {
 	cout << endl << "-> N: " << N;
 	cout << endl << "-> mode: " << mode;
 
+	vector <TicketOffice> vMyTicketOffice;
+
+
 	if (mode == "create") {
-		createMode(fileName);
+		vMyTicketOffice = inputMode(fileName, N);
+		printMode(vMyTicketOffice);
 	}
 	if (mode == "read") {
-		readMode(fileName);
+		readMode(fileName, vMyTicketOffice, N);
 	}
 
 	return 0;
