1dc8e91277c4558d006da1ea2ad928e1f8b21d3b
Baranov Pavel <pavelbaranow@gmail.com>
2019-03-19 13:11:51 +0300

    Update TCharArray.h

--- II semester/task 4/TCharArray.h
+++ II semester/task 4/TCharArray.h
@@ -2,32 +2,28 @@
 
 #include <iostream>
 
-using std::ostream;
-
 class TCharArray
 {
-private:
-	int  len;
-	char *pMem;
-
 protected:
-	size_t GetStrLen(const char* str);
+	char *pMem;
+	size_t  len;
 
 public:
 	TCharArray(const char* arr);
-	TCharArray(const int len);
+	TCharArray(const int len);//+
 	TCharArray(const TCharArray &arr);//КК
 	TCharArray();//КПУ
 	~TCharArray();
-	int Size(void) const;
+
+	size_t GetStrLen(const char* str);
 	char& At(const int n) const;
 	char& operator[] (const int n) const;
 	int operator==(const TCharArray &arr) const;
 	int operator!=(const TCharArray &arr) const;
-	friend ostream &operator<<(ostream &ostr, const TCharArray &arr);
 	TCharArray& operator=(const TCharArray &arr);
 	TCharArray operator+ (const TCharArray &arr);
 	int operator<(const TCharArray &arr) const;
 	int operator>(const TCharArray &arr) const;
+	
 };
 
