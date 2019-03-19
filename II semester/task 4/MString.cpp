373d08edb17b9bdcbd64ca9179a75ca2619138dc
Baranov Pavel <pavelbaranow@gmail.com>
2019-03-19 13:31:39 +0300

    stabile

--- II semester/task 4/MString.cpp
+++ II semester/task 4/MString.cpp
@@ -1,33 +1,25 @@
 #include "pch.h"
 #include "MString.h"
 
-using  std::endl;
-using  std::cout;
-
 MString::MString(const char* arr) : TCharArray(arr)
 {
-	//cout << endl << "!!!DBG MString: Конструктор (const char* arr)" << endl;
 }
 
 MString::MString() : TCharArray()
 {
-	//cout << endl << "!!!DBG MString: Конструктор по умолчанию" << endl;
 }
 
 MString::~MString()
 {
-	//cout << endl << "!!!DBG MString: Деструктор" << endl;
 }
 
 size_t MString::Size(void) const
 {
-	//cout << endl << "!!!DBG MString: Size" << endl;
 	return len;
 }
 
 ostream& operator<<(ostream &ostr, const MString &arr)
 {
-	//cout << endl << "!!!DBG MString: <<";
 	for (size_t i = 0; i < arr.len; i++) {
 		ostr << arr.pMem[i];
 	}
