f8d68e8c69bfc30ea37d2f0e55c284c127b77e54
Baranov Pavel <pavelbaranow@gmail.com>
2019-04-25 13:26:20 +0300

    Update graph.cpp

--- II semester/graph.cpp
+++ II semester/graph.cpp
@@ -16,14 +16,12 @@ struct Top
 	int number;
 	int size;
 	vector<int> neighbors;
-	int color;
+	string color;
 
 	bool operator <(const Top &rhs) {
-		return size < rhs.size;
+		return size > rhs.size;
 	}
 };
-
-
 
 vector<vector<int>> readMode(string fileName) {
 	cout << endl << endl << "-> =R=E=A=D= =M=O=D=E";
@@ -90,13 +88,152 @@ void printModeGraph(vector<Top> graph) {
 	cout << endl;
 }
 
-vector<Top> coloring(vector<Top> graph) {
+vector<Top> coloring(vector<Top> igraph) {
 	cout << endl << endl << "-> =C=O=L=O=R=I=N=G= =G=R=A=P=H=";
+	vector<Top> sgraph = igraph;
+	sort(sgraph.begin(), sgraph.end());
 
-	sort(graph.begin(), graph.end());
+	//Red blue green white 
+
+	//RED ZONE
+	{
+		sgraph[0].color = "Red";
+		int index = sgraph[0].number;
+		igraph[index].color = "Red";
+
+		for (int i = 0; i < sgraph.size(); i++) {
+			bool flag = true;
+
+			for (int j = 0; j < sgraph[i].neighbors.size(); j++) {
+
+				if (sgraph[i].neighbors[j] == index) {
+					flag = false;
+				}
+
+				if (igraph[sgraph[i].neighbors[j]].color == "Red") {
+					flag = false;
+				}
+			}
+			if (flag) {
+				sgraph[i].color = "Red";
+				igraph[sgraph[i].number].color = "Red";
+			}
+		}
+	}
+	//RED ZONE END
+
+	//BLUE ZONE
+	{
+		int index = -1;
+		for (int i = 0; i < sgraph.size(); i++) {
+			if (sgraph[i].color == "-1") {
+				index = i;
+				break;
+			}
+		}
+
+		if (index == -1) {
+			cout << endl << "Job done";
+			return igraph;
+		}
+
+		sgraph[index].color = "Blue";
+		index = sgraph[index].number;
+		igraph[index].color = "Blue";
+
+
+		for (int i = 0; i < sgraph.size(); i++) {
+			bool flag = true;
+			for (int j = 0; j < sgraph[i].neighbors.size(); j++) {
+				if (sgraph[i].neighbors[j] == index) {
+					flag = false;
+				}
+				if (igraph[sgraph[i].neighbors[j]].color == "Blue") {
+					flag = false;
+				}
+			}
+			if ((flag) && (sgraph[i].color == "-1")) {
+				sgraph[i].color = "Blue";
+				igraph[sgraph[i].number].color = "Blue";
+			}
+		}
+	}
+	//BLUE ZONE END
+
+	//GREEN ZONE
+	{
+		int index = -1;
+		for (int i = 0; i < sgraph.size(); i++) {
+			if (sgraph[i].color == "-1") {
+				index = i;
+				break;
+			}
+		}
+
+		if (index == -1) {
+			cout << endl << "Job done";
+			return igraph;
+		}
+		sgraph[index].color = "green";
+		index = sgraph[index].number;
+		igraph[index].color = "green";
+
+		for (int i = 0; i < sgraph.size(); i++) {
+			bool flag = true;
+			for (int j = 0; j < sgraph[i].neighbors.size(); j++) {
+				if (sgraph[i].neighbors[j] == index) {
+					flag = false;
+				}
+				if (igraph[sgraph[i].neighbors[j]].color == "green") {
+					flag = false;
+				}
+			}
+			if ((flag) && (sgraph[i].color == "-1")) {
+				sgraph[i].color = "green";
+				igraph[sgraph[i].number].color = "green";
+			}
+		}
+	}
+	//GREEN ZONE END
+
+	//WHITE ZONE
+	{
+		int index = -1;
+		for (int i = 0; i < sgraph.size(); i++) {
+			if (sgraph[i].color == "-1") {
+				index = i;
+				break;
+			}
+		}
+
+		if (index == -1) {
+			cout << endl << "Job done";
+			return igraph;
+		}
+		sgraph[index].color = "white";
+		index = sgraph[index].number;
+		igraph[index].color = "white";
+
+		for (int i = 0; i < sgraph.size(); i++) {
+			bool flag = true;
+			for (int j = 0; j < sgraph[i].neighbors.size(); j++) {
+				if (sgraph[i].neighbors[j] == index) {
+					flag = false;
+				}
+				if (igraph[sgraph[i].neighbors[j]].color == "white") {
+					flag = false;
+				}
+			}
+			if ((flag) && (sgraph[i].color == "-1")) {
+				sgraph[i].color = "white";
+				igraph[sgraph[i].number].color = "white";
+			}
+		}
+	}
+	//WHITE ZONE END
 
 	cout << endl << "Job done";
-	return graph;
+	return igraph;
 }
 
 int main()
@@ -114,7 +251,7 @@ int main()
 		Top tmp;
 		tmp.number = i;
 		tmp.size = inputData[i].size();
-		tmp.color = -1;
+		tmp.color = "-1";
 		tmp.neighbors = inputData[i];
 		graph.push_back(tmp);
 	}
@@ -125,6 +262,6 @@ int main()
 	coloringGraph = coloring(graph);
 	printModeGraph(coloringGraph);
 
-    return 0;
+	return 0;
 }
 
